de2266a6ca8925491437d39e1b6c60392476e56b
Kapil Agarwal <kaagarw@microsoft.com>
2016-07-15 11:54:00 -0700

    Initial implementation of Dynamic object validator

---
+++ DynamicObjectValidator/DynamicObjectValidator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Dynamic;
+using System.Reflection;
+
+public static class DynamicObjectValidator
+{
+    public static bool Validate<T>(dynamic dynamicObject, T typedObject)
+    {
+        if (dynamicObject == null && typedObject == null)
+        {
+            return true;
+        }
+        else if ((dynamicObject == null && typedObject != null) || (dynamicObject != null && typedObject == null))
+        {
+            return false;
+        }
+        else
+        {
+            if (dynamicObject is IDynamicMetaObjectProvider)
+            {
+                bool passed = true;
+                //IEnumerable<string> members = dynamicObject.GetDynamicMemberNames();
+                //foreach (var prop in members)
+                foreach (KeyValuePair<string, object> kvp in dynamicObject)
+                {
+                    string prop = kvp.Key;
+                    var typedType = typedObject.GetType();
+                    var typedProp = typedObject.GetType().GetField(prop);
+                    Type nestedType = typedProp.GetValue(typedObject).GetType();
+                    MethodInfo method = typeof(DynamicObjectValidator).GetMethod("Validate").MakeGenericMethod(new Type[] {nestedType});
+                    var typedValue = Convert.ChangeType(typedProp.GetValue(typedObject), nestedType);
+                    var dynamicValue = kvp.Value;
+                    if (typedProp != null)
+                    {
+                        passed= (bool) method.Invoke(null, new object[] {dynamicValue, typedValue});
+                        if (!passed)
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return (dynamicObject == typedObject);
+            }
+        }
+        return true;
+    }
+}
---
+++ DynamicObjectValidator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validator
+{
+    public class TestClass2
+    {
+        public string AA;
+        public int BB;
+    }
+
+    public class TestClass
+    {
+        public string A;
+        public TestClass2 B;
+    }
+
+    public static class Test1
+    {
+        public static void Test()
+        {
+            dynamic a = new ExpandoObject();
+            a.A = "hi";
+            a.B = new ExpandoObject();
+            a.B.AA = "bye";
+            a.B.BB = 12;
+
+            TestClass b = new TestClass();
+            b.A = "hi";
+            b.B = new TestClass2();
+            b.B.AA = "bye";
+            b.B.BB = 12;
+
+            Console.Write(DynamicObjectValidator.Validate<TestClass>(a, b));
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1.Test();
+            Console.ReadLine();
+        }
+
+
+    }
+}
